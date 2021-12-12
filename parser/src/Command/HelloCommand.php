c6edf47686909bae21e16dd2ba15c7c5f76b4df9
Valentin <medvedev152@live.com>
2021-12-12 21:47:56 +1000

    переместил докер конфиги, исправил работу эндпоинтоа и команды

--- parser/src/Command/HelloCommand.php
+++ parser/src/Command/HelloCommand.php
@@ -1,15 +1,16 @@
 <?php
 
+namespace App\Command;
+
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class HelloCommand extends Command
 {
-    protected static $defaultName = 'parser:hello';
+    protected static $defaultName = 'app:hello';
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('Hello world!');
 
